2994d19a026fa662cd870450070c90835e41076a
synsi <mail@himitsu.dev>
2023-05-03 11:56:06 +0900

    fix problems with jens

--- breaktime_saldo_updates.py
+++ breaktime_saldo_updates.py
@@ -61,6 +61,29 @@ def run_flextime_for_day(day:date):
     update_flextime(empl, day)
 
 
+def run_flextime_for_jens():
+    empl = Angestellter.get_all_active("ANGESTELLTER")
+    empl = [e for e in empl if e._id == 28]
+    update_flextime(empl, date(2023, 3, 15))
+    update_flextime(empl, date(2023, 4, 1))
+
+
+def run_past_breaktime_for_jens():
+    # run the check for the past 90 days
+    today = date.today()
+    #minus90 = today - timedelta(days=90)
+    plus1 = timedelta(days=1)
+    empl = Angestellter.get_all_active("ANGESTELLTER") 
+    empl = [e for e in empl if e._id == 28]
+    for ma in empl:
+        firstday = ma.get_first_record_date()
+        calcday = firstday
+        while calcday < today:
+            ma.update_breaktime(calcday)
+            calcday += plus1
+
+
+
 if __name__ == "__main__":
     thisfile = Path(__file__)
     logging.basicConfig(filename=str(thisfile.parent.parent.resolve() / f"kimai2_autohire_{thisfile.stem}.log"),
@@ -77,4 +100,6 @@ if __name__ == "__main__":
     #run_breaktime_update_90_days()
     #run_flextime_for_day(date(2022, 11, 23))
     #run_flextime_for_day(date(2023, 2, 20))
-    run_flextime_for_day(date(2023, 3, 23))
+    #run_flextime_for_day(date(2023, 3, 23))
+    run_past_breaktime_for_jens()
+    run_flextime_for_jens()
